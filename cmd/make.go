a5d9d4d305390b9668c4c3fb6e1e4f3d6cdd84a1
Karn Wong <karn@karnwong.me>
2024-07-28 19:14:54 +0700

    feat: add make

---
+++ cmd/make.go
@@ -0,0 +1,21 @@
+/*
+Copyright © 2024 Karn Wong <karn@karnwong.me>
+*/
+package cmd
+
+import (
+	"github.com/kahnwong/config-init/template"
+	"github.com/spf13/cobra"
+)
+
+var makeCmd = &cobra.Command{
+	Use:   "make",
+	Short: "Init Makefile",
+	Run: func(cmd *cobra.Command, args []string) {
+		template.WriteConfig("make", "Makefile", "Makefile")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(makeCmd)
+}
