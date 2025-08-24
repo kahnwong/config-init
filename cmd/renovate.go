c96f6d3dd299bf331ccc21627fcb6807555d8240
Karn Wong <karn@karnwong.me>
2025-08-24 10:53:03 +0700

    feat(renovate): init

---
+++ cmd/renovate.go
@@ -0,0 +1,22 @@
+/*
+Copyright © 2024 Karn Wong <karn@karnwong.me>
+*/
+package cmd
+
+import (
+	"github.com/kahnwong/config-init/template"
+
+	"github.com/spf13/cobra"
+)
+
+var renovateCmd = &cobra.Command{
+	Use:   "renovate",
+	Short: "Init renovate ci config",
+	Run: func(cmd *cobra.Command, args []string) {
+		template.WriteConfig("renovate", "renovate.json", "renovate.json")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(renovateCmd)
+}
